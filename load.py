792d50c42285146354cf1a4d6fbd3eea7c016e92
NOTEBOOK-L07\user <terenevichlollegend@gmail.com>
2023-02-11 15:16:41 +0300

    почти

--- load.py
+++ load.py
@@ -1,7 +1,11 @@
 import pygame
-player_image = pygame.image.load("image/googe_dino.png").convert_alpha()
+player_image = [pygame.image.load("image/1 (1).png").convert_alpha(),
+                pygame.image.load("image/2 (1).png").convert_alpha()]
+player_sit_image = pygame.image.load("image/1_1.png").convert_alpha()
 kaktys_image = pygame.image.load("image/kaktus.png").convert_alpha()
+kaktys2_image = pygame.image.load("image/kaktus_2.png").convert_alpha()
 earth_image = pygame.image.load("image/earth.png").convert_alpha()
 menu_image = pygame.image.load("image/dino swords.jpg").convert_alpha()
 space_image = pygame.image.load("image/space.png").convert_alpha()
 sword_image = pygame.image.load("image/sword.png").convert_alpha()
+cloud_image = pygame.image.load("image/cloud.png").convert_alpha()
--- main.py
+++ main.py
@@ -14,6 +14,9 @@ FPS=60
 lvl="menu"
 sc=pygame.display.set_mode((WIDTH, HEIGHT))
 clock=pygame.time.Clock()
+font = pygame.font.SysFont("arial", 40)
+score = 0
+time = 10
 
 from load import *
 
@@ -25,15 +28,14 @@ def startMenu():
     key = pygame.key.get_pressed()
     if key[pygame.K_SPACE]:
         lvl = "Game"
-    #sc.blit(start_image, (100, 200))
-    #sc.blit(records_image, (100, 300))
-    #sc.blit(exit_image, (100, 400))
-    #pos_mouse=pygame.mouse.get_pos()
     pygame.display.update()
 
 def game_lvl():
+    global records_list, score
     sc.fill((128, 128, 128))
     sc.blit(earth_image, (0, HEIGHT - 100))
+    sc.blit(cloud_image, (200, 70))
+    sc.blit(cloud_image, (800, 50))
     player_group.update()
     player_group.draw(sc)
     earth_group.update()
@@ -42,22 +44,35 @@ def game_lvl():
     kaktys_group.draw(sc)
     sword_group.update()
     sword_group.draw(sc)
+    with open("score.txt", "r", encoding="utf-8") as file:
+        records_list = []
+        for i in range(5):
+            records_list.append(file.readline().replace("\n", ""))
+    text_font = font.render(records_list[0], True, "black")
+    sc.blit(text_font, (1000, 50))
+    with open("score.txt", "w", encoding="utf-8") as file:
+        file.write(str(score) + "\n")
     pygame.display.update()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, image, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
+        self.image = image[0]
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.bottom = HEIGHT - 40
         self.jump = False
         self.jump_step = -22
-        self.timer_spawn = 0
+        self.timer_spawn_kaktus = 0
+        self.timer_spawn_kaktus2 = 0
+        self.timer_spawn_sword = 0
         self.mask = pygame.mask.from_surface(self.image)
         self.mask_outline = self.mask.outline()
         self.mask_list = []
+        self.frame = 0
+        self.timer_anime = 0
+        self.anime = True
     def update(self):
         global FPS
         key = pygame.key.get_pressed()
@@ -70,16 +85,23 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.jump = False
                 self.jump_step = -22
-        self.timer_spawn += 1
-        self.timer_spawn += 1
-        if self.timer_spawn / FPS > random.randint(3, 6):
+        if key[pygame.K_DOWN]:
+            self.image = player_sit_image
+        self.timer_spawn_kaktus += 1
+        self.timer_spawn_kaktus2 += 1
+        self.timer_spawn_sword += 1
+        if self.timer_spawn_kaktus / FPS > time:
             kaktys = Kaktys(kaktys_image)
             kaktys_group.add(kaktys)
-            self.timer_spawn = 0
-        if self.timer_spawn / FPS > 4:
+            self.timer_spawn_kaktus = 0
+        if self.timer_spawn_kaktus2 / FPS > time - 2:
+            kaktys2 = Kaktys(kaktys2_image)
+            kaktys_group.add(kaktys2)
+            self.timer_spawn_kaktus2 = 0
+        if self.timer_spawn_sword / FPS > time + 15:
             sword = Sword(sword_image)
             sword_group.add(sword)
-            self.timer_spawn = 0
+            self.timer_spawn_sword = 0
         self.mask_list = []
         for i in self.mask_outline:
             self.mask_list.append((i[0] + self.rect.x, i[1] + self.rect.y))
@@ -87,6 +109,15 @@ class Player(pygame.sprite.Sprite):
         #    x = point[0]
         #    y = point[1]
         #    pygame.draw.circle(sc, "red", (x, y), 3)
+        if self.anime:
+            self.timer_anime += 1
+            if self.timer_anime / FPS > 0.1:
+                if self.frame == len(player_image) - 1:
+                    self.frame = 0
+                else:
+                    self.frame += 1
+                self.timer_anime = 0
+        self.image = player_image[self.frame]
 
 
 class Kaktys(pygame.sprite.Sprite):
@@ -101,7 +132,7 @@ class Kaktys(pygame.sprite.Sprite):
         self.mask_outline = self.mask.outline()
         self.mask_list = []
     def update(self):
-        global FPS
+        global FPS, sword
         self.rect.x -= self.speed
         self.mask_list = []
         for i in self.mask_outline:
@@ -119,7 +150,7 @@ class Sword(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(WIDTH, WIDTH + 500)
-        self.rect.bottom = HEIGHT - 40
+        self.rect.bottom = HEIGHT - 250
         self.speed = 10
         self.mask = pygame.mask.from_surface(self.image)
         self.mask_outline = self.mask.outline()
@@ -134,6 +165,9 @@ class Sword(pygame.sprite.Sprite):
         #    x = point[0]
         #    y = point[1]
         #    pygame.draw.circle(sc, "blue", (x, y), 3)
+        if len(set(self.mask_list) & set(player.mask_list)) > 0:
+            self.kill()
+
 
 
 def restart():
@@ -158,4 +192,6 @@ while True:
         game_lvl()
     elif lvl == "menu":
         startMenu()
+    score += 1
+    time -= 0.001
     clock.tick(FPS)
